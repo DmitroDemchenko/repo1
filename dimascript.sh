1139ef41dbf0b5b38b6be170b2be5211290b9aaf
Dmitro.Demchenko <dimadema92@gmail.com>
2018-01-24 23:25:39 +0000

    add dimascript

---
+++ dimascript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+helper(){
+
+cat<<EOF
+	SEE BELOW WHAT ARGUMENTS YOU CAN USE:
+
+ -p  --  look path to file;
+ -d  --  look your name;
+ -t  --  available MEM	
+ 
+
+EOF
+
+}
+
+
+
+helper
+
